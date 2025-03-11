47b05826c6de1aa26ae8ef8f7e4f1f06d7164a2d
Aman Sagar <amansagar0307@gmail.com>
2025-03-12 02:04:42 +0530

    update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   images: { unoptimized: true },
+// };
+
+// module.exports = nextConfig;
+
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true, 
+  skipWaiting: true, 
+  disable: process.env.NODE_ENV === 'development',
+});
+
 const nextConfig = {
   output: 'export',
   eslint: {
@@ -7,4 +25,4 @@ const nextConfig = {
   images: { unoptimized: true },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
